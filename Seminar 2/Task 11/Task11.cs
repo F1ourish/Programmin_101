60e77bb8f7f039bbc414c9f179b877de17d9f8a7
Novozhilov Alexandr <gofdeadpool@gmail.com>
2023-09-24 22:27:13 +0300

    Выполненные ДЗ + семинар

---
+++ Seminar 2/Task 11/Task11.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = Random.Shared.Next(100, 1000);
+Console.WriteLine($"Сгенерировано число {num}.");
+int firstDigit = num / 100;
+int secDigit = num % 10;
+int result = firstDigit * 10 + secDigit;
+Console.WriteLine($"Полученное число: {result}.");
---
+++ Seminar 2/Task9/Task9.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+using System.Globalization;
+
+int num = Random.Shared.Next(10, 100);
+Console.WriteLine(num);
+int lastDigit = num % 10;
+int firstDigit = num / 10;
+
+if (lastDigit < firstDigit) Console.WriteLine($"Первая цифра больше второй и равна {firstDigit}.");
+else if(lastDigit > firstDigit) Console.WriteLine($"Вторая цифра больше первой и равна {lastDigit}.");
+else Console.WriteLine("Цифры равны.");
